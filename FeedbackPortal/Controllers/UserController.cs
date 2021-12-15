8587e2141783a79a3c7991c2b95c0bb40b2b2326
JayeshChaware <jayesh.chaware-imed@bvp.edu.in>
2021-12-15 23:50:03 +0530

    15 dec assignment

---
+++ FeedbackPortal/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FeedbackPortal.Models;
+
+namespace FeedbackPortal.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public static List<User> users =new List<User>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
+        {
+            
+            return View(users);
+        }
+
+        [HttpGet]
+        [Route("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public IActionResult Create(User user)
+        {
+            users.Add(user);
+            return RedirectToAction("Get", users);
+        }
+
+        [HttpGet]
+        [Route("Edit")]
+        public IActionResult Edit(int id)
+        {
+            User User = users.Where(use => use.Id.Equals(id)).Select(use => use).FirstOrDefault();
+            return View();
+        }  
+
+        [HttpPost]
+        [Route("Edit")]
+        public IActionResult Edit(User user)
+        {
+            User User = users.Where(use => use.Id == user.Id).FirstOrDefault();
+            if (User != null)
+            {
+                User.Name = user.Name;
+                User.IsActive = user.IsActive;
+            };
+            return RedirectToAction("Get", users);
+        }
+
+        [HttpGet]
+        [Route("Details")] // Display Details
+        public IActionResult Details(int id)
+        {
+            User User = users.Where(use => use.Id.Equals(id)).Select(u => u).FirstOrDefault();
+            return View(User);
+        }
+
+        [Route("Delete")]
+        public IActionResult Delete(int id)
+        {
+            User user = users.Find(use => use.Id == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                return RedirectToAction("Index");
+            }
+            
+            return View("Index");
+
+        }
+    }
+}
