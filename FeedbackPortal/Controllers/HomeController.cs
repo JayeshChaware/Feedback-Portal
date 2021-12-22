74396857cb6f2b52c3d5c5f697ce1579ce0a34a6
JayeshChaware <jayesh.chaware-imed@bvp.edu.in>
2021-12-22 12:31:56 +0530

    commit 22 dec 21

--- FeedbackPortal/Controllers/HomeController.cs
+++ FeedbackPortal/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
 ﻿using FeedbackPortal.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace FeedbackPortal.Controllers
 {
